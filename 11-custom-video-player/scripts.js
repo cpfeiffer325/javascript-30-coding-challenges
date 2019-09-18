ccbfec7d489deedc97b859aef3fab4b6b2ddfad2
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-09-17 22:05:05 -0600

    add functionality to handle volume, video speed and skip forward and back

--- 11-custom-video-player/scripts.js
+++ 11-custom-video-player/scripts.js
@@ -4,8 +4,8 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
-const range = player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 // Build out function
 function togglePlay() {
@@ -21,11 +21,26 @@ function updateButton() {
     const icon = this.paused ? '►' : '❚ ❚';
     console.log(icon);
     toggle.textContent = icon;
-  }
+}
+function skip() {
+    console.log('Skipping');
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+    console.log(this.value);
+}
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
 
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
