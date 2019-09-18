5d00161b13955bd861bfd8ec0cea16f8bdf0af44
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-09-17 22:12:32 -0600

    create functio nto select video along progress bar

--- 11-custom-video-player/scripts.js
+++ 11-custom-video-player/scripts.js
@@ -34,6 +34,12 @@ function handleProgress() {
     const percent = (video.currentTime / video.duration) * 100;
     progressBar.style.flexBasis = `${percent}%`;
 }
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+// Additional task, add button to make the video go full screen
+
 
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
@@ -44,3 +50,11 @@ video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
