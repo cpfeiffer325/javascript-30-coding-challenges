9cca164f6ddf60b6791ea088edb2f217ec789243
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-09-22 23:12:49 -0600

    add functionality for red green blue color effects

--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -25,6 +25,17 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+
+        // take pixels out
+        let pixels = ctx.getImageData(0, 0 , width, height);
+
+        // mess with them
+        // pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
+        ctx.globalAlpha = 0.5;
+
+        // put them back
+        ctx.putImageData(pixels, 0, 0)
     }, 16);
 }
 
@@ -39,6 +50,24 @@ function takePhoto() {
     link.setAttribute('download', 'handsome');
     link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
     strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+      pixels.data[i + 0] = pixels.data[i + 0] - 200; // RED
+      pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+      pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; // RED
+        pixels.data[i + 400] = pixels.data[i + 1]; // GREEN
+        pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+      }
+      return pixels;     
 }
 
 getVideo();
