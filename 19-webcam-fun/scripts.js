34d3bea220fd83661068df763fa86706b959b967
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-09-22 23:19:33 -0600

    complete green screen function

--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -31,8 +31,11 @@ function paintToCanvas() {
 
         // mess with them
         // pixels = redEffect(pixels);
-        pixels = rgbSplit(pixels);
-        ctx.globalAlpha = 0.5;
+        
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.5;
+
+        pixels = greenScreen(pixels);
 
         // put them back
         ctx.putImageData(pixels, 0, 0)
@@ -66,8 +69,35 @@ function rgbSplit(pixels) {
         pixels.data[i - 150] = pixels.data[i + 0]; // RED
         pixels.data[i + 400] = pixels.data[i + 1]; // GREEN
         pixels.data[i - 550] = pixels.data[i + 2]; // Blue
-      }
-      return pixels;     
+    }
+    return pixels;     
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+      levels[input.name] = input.value;
+    });
+  
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+    
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            // take it out!
+            pixels.data[i + 3] = 0;
+        }
+    }
+  
+    return pixels;   
 }
 
 getVideo();
