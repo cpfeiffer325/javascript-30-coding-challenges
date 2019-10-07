de67455e7dd9b7389fdc0856f92fa6675b732036
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-10-06 19:42:52 -0600

    create  display endtime and start timer function

--- 29-countdown-timer/scripts.js
+++ 29-countdown-timer/scripts.js
@@ -1,6 +1,8 @@
 let countdown
 
 const timerDisplay = document.querySelector('.display__time-left')
+const endTime = document.querySelector('.display__end-time')
+const buttons = document.querySelectorAll('[data-time]')
 
 function timer(seconds) {
   // clear any existing timers
@@ -9,7 +11,7 @@ function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
-  // displayEndTime(then);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -27,5 +29,21 @@ function displayTimeLeft(seconds){
   const minutes = Math.floor(seconds / 60)
   const remainderSeconds = seconds % 60
   const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
+  document.title = display
   timerDisplay.textContent = display
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp)
+  const hour = end.getHours()
+  const adjustedHour = hour > 12 ? hour - 12 : hour
+  const minutes = end.getMinutes()
+  // endTime.textContent = `Be Back at ${hour}:${minutes}` // european clock
+  endTime.textContent = `Be Back at ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time)
+  timer(seconds)
+}
+
