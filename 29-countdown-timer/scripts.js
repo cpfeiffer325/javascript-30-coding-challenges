8d6ce121b67fc0685fb7c611a7403071992c8afd
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-10-06 19:21:29 -0600

    create time display function

---
+++ 29-countdown-timer/scripts.js
@@ -0,0 +1,31 @@
+let countdown
+
+const timerDisplay = document.querySelector('.display__time-left')
+
+function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
+
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  // displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop it!
+    if(secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    // display it
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds){
+  const minutes = Math.floor(seconds / 60)
+  const remainderSeconds = seconds % 60
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
+  timerDisplay.textContent = display
+}
